61aa9699262d65292fd3184007b3d5e43a2d53f9
David Barroso <dbarrosop@dravetech.com>
2017-03-26 12:18:55 +0200

    Fixed manifest file (#21)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-yang",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     author="David Barroso",
     author_email="dbarrosop@dravetech.com",
