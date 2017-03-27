3a984b02df800b189b7fc8224896d9ceebe95986
David Barroso <dbarrosop@dravetech.com>
2017-03-27 14:17:16 +0200

    Add examples

--- napalm_yang/jinja_filters/ip_filters.py
+++ napalm_yang/jinja_filters/ip_filters.py
@@ -9,10 +9,20 @@ def filters():
 
 
 def netmask_to_cidr(value):
-    """ Converts a network mask to it's CIDR value. """
+    """
+    Converts a network mask to it's CIDR value.
+
+    Examples:
+        >>> "{{ '255.255.255.0'|netmask_to_cidr }}" -> "24"
+    """
     return netaddr.IPAddress(value).netmask_bits()
 
 
 def cidr_to_netmask(value):
-    """ Converts a CIDR prefix-length to a network mask. """
+    """
+    Converts a CIDR prefix-length to a network mask.
+
+    Examples:
+        >>> "{{ '24'|cidr_to_netmask }}" -> "255.255.255.0"
+    """
     return netaddr.IPNetwork("1.1.1.1/{}".format(value)).netmask
