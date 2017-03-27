00ce68ab04ec3bedc97f944c422e920e07ded254
David Barroso <dbarrosop@dravetech.com>
2017-03-27 12:21:01 +0200

    Added defailt_only action on TextTranslator

--- napalm_yang/translators/text.py
+++ napalm_yang/translators/text.py
@@ -27,6 +27,10 @@ class TextTranslator(XMLTranslator):
         mapping["element"] = "command"
         super()._parse_leaf_element(attribute, model, other, mapping, translation, force)
 
+    def _init_element_default_only(self, attribute, model, other, mapping, translation):
+        # There is nothing to do here, default_only action is only useful to remove lists
+        return translation
+
     def _init_element_container(self, attribute, model, other, mapping, translation):
         mapping["key_element"] = "command"
         mapping["container"] = model._yang_name
@@ -46,6 +50,11 @@ class TextTranslator(XMLTranslator):
 
         e = etree.SubElement(translation, "command")
         e.text = mapping["negate"]
+
+    def _default_element_default_only(self, mapping, translation, replacing):
+        if self.replace:
+            return
+        self._default_element_container(mapping, translation, replacing)
 
     def _xml_to_text(self, xml, text=""):
         for element in xml:
