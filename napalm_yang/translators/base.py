48c5137fbcafde3f86e3e28bd9e88eb7c357cba5
David Barroso <dbarrosop@dravetech.com>
2017-05-01 19:50:24 +0200

    Find translation point per rule as before

--- napalm_yang/translators/base.py
+++ napalm_yang/translators/base.py
@@ -1,38 +1,54 @@
+def _find_translation_point(rule, bookmarks, translation):
+    if "in" in rule.keys():
+        t = bookmarks
+        for p in rule["in"].split("."):
+            try:
+                t = t[p]
+            except TypeError:
+                t = t[int(p)]
+            translation = t
+    return translation
+
+
 class BaseTranslator(object):
 
     def __init__(self, merge, replace):
         self.merge = merge
         self.replace = replace
 
-    def init_element(self, attribute, model, other, mapping, translation):
+    def init_element(self, attribute, model, other, mapping, translation, bookmarks):
         for m in mapping:
+            t = _find_translation_point(m, bookmarks, translation)
             method_name = "_init_element_{}".format(m["mode"])
-            t = getattr(self, method_name)(attribute, model, other, m, translation)
-            if t is False:
+            et = getattr(self, method_name)(attribute, model, other, m, t)
+            if et is False:
                 # if it's False we want to return None to signal we want to abort
                 return None
-            elif t is not None:
-                return t
+            elif et is not None:
+                return et
 
-    def default_element(self, mapping, translation, replacing=False):
+    def default_element(self, mapping, translation, bookmarks, replacing=False):
         for m in mapping:
+            t = _find_translation_point(m, bookmarks, translation)
             method_name = "_default_element_{}".format(m["mode"])
-            getattr(self, method_name)(m, translation, replacing)
+            getattr(self, method_name)(m, t, replacing)
 
-    def translate_leaf(self, attribute, model, other, mapping, translation):
+    def translate_leaf(self, attribute, model, other, mapping, translation, bookmarks):
         for m in mapping:
+            t = _find_translation_point(m, bookmarks, translation)
             method_name = "_translate_leaf_{}".format(m["mode"])
-            getattr(self, method_name)(attribute, model, other, m, translation)
+            getattr(self, method_name)(attribute, model, other, m, t)
 
-    def translate_container(self, attribute, model, other, mapping, translation):
+    def translate_container(self, attribute, model, other, mapping, translation, bookmarks):
         for m in mapping:
+            t = _find_translation_point(m, bookmarks, translation)
             method_name = "_translate_container_{}".format(m["mode"])
-            t = getattr(self, method_name)(attribute, model, other, m, translation)
-            if t is False:
+            et = getattr(self, method_name)(attribute, model, other, m, t)
+            if et is False:
                 # if it's False we want to return None to signal we want to abort
                 return None
-            elif t is not None:
-                return t
+            elif et is not None:
+                return et
 
     def _translate_leaf_skip(self, attribute, model, other, mapping, translation):
         return translation
