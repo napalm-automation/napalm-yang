781fc4bc02785b1a39b50d9798425dd615d83070
David Barroso <dbarrosop@dravetech.com>
2017-05-05 13:54:52 +0200

    Minor fixes to the logic of the lists

--- napalm_yang/translators/base.py
+++ napalm_yang/translators/base.py
@@ -18,6 +18,11 @@ class BaseTranslator(object):
 
     def init_element(self, attribute, model, other, mapping, translation, bookmarks):
         for m in mapping:
+            if m["mode"] == "skip":
+                continue
+            elif m["mode"] == "gate":
+                return
+
             t = _find_translation_point(m, bookmarks, translation)
             method_name = "_init_element_{}".format(m["mode"])
             et = getattr(self, method_name)(attribute, model, other, m, t)
@@ -26,21 +31,37 @@ class BaseTranslator(object):
                 return None
             elif et is not None:
                 return et
+        return translation
 
     def default_element(self, mapping, translation, bookmarks, replacing=False):
         for m in mapping:
+            if m["mode"] == "skip":
+                continue
+            elif m["mode"] == "gate":
+                return
+
             t = _find_translation_point(m, bookmarks, translation)
             method_name = "_default_element_{}".format(m["mode"])
             getattr(self, method_name)(m, t, replacing)
 
     def translate_leaf(self, attribute, model, other, mapping, translation, bookmarks):
         for m in mapping:
+            if m["mode"] == "skip":
+                continue
+            elif m["mode"] == "gate":
+                return
+
             t = _find_translation_point(m, bookmarks, translation)
             method_name = "_translate_leaf_{}".format(m["mode"])
             getattr(self, method_name)(attribute, model, other, m, t)
 
     def translate_container(self, attribute, model, other, mapping, translation, bookmarks):
         for m in mapping:
+            if m["mode"] == "skip":
+                continue
+            elif m["mode"] == "gate":
+                return
+
             t = _find_translation_point(m, bookmarks, translation)
             method_name = "_translate_container_{}".format(m["mode"])
             et = getattr(self, method_name)(attribute, model, other, m, t)
@@ -49,17 +70,4 @@ class BaseTranslator(object):
                 return None
             elif et is not None:
                 return et
-
-    def _translate_leaf_skip(self, attribute, model, other, mapping, translation):
         return translation
-
-    _init_element_skip = _translate_leaf_skip
-    _translate_leaf_gate = _translate_leaf_skip
-
-    def _translate_container_gate(self, attribute, model, other, mapping, translation):
-        return None
-
-    _translate_container_skip = _translate_leaf_skip
-
-    def _default_element_skip(self, mapping, translation, replacing):
-        return None
