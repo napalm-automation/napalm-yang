d15816c2f1c787f61fafdae7e345949f4fadc940
David Barroso <dbarrosop@dravetech.com>
2017-05-06 20:53:28 +0200

    Added config parser for network-instances/junos

--- test/integration/test_profiles.py
+++ test/integration/test_profiles.py
@@ -35,6 +35,7 @@ BASE_PATH = os.path.dirname(__file__)
 test_config_profile_models = [
     ["ios", napalm_yang.models.openconfig_interfaces, "default"],
     ["eos", napalm_yang.models.openconfig_network_instance, "default"],
+    ["junos", napalm_yang.models.openconfig_network_instance, "default"],
 ]
 
 test_state_profile_models = [
