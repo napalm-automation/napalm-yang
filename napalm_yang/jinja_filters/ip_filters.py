4b371d1061b0cdec9877727cb762aa7289af7c2f
Matej Vadnjal <matej.vadnjal@arnes.si>
2017-06-12 16:09:16 +0200

    add normalize_address jinja filter and rename addrmask_to_cidr to normalize_prefix

--- napalm_yang/jinja_filters/ip_filters.py
+++ napalm_yang/jinja_filters/ip_filters.py
@@ -5,7 +5,8 @@ def filters():
     return {
         "netmask_to_cidr": netmask_to_cidr,
         "cidr_to_netmask": cidr_to_netmask,
-        "addrmask_to_cidr": addrmask_to_cidr,
+        "normalize_prefix": normalize_prefix,
+        "normalize_address": normalize_address,
     }
 
 
@@ -29,13 +30,33 @@ def cidr_to_netmask(value):
     return netaddr.IPNetwork("1.1.1.1/{}".format(value)).netmask
 
 
-def addrmask_to_cidr(value):
+def normalize_prefix(value):
     """
-    Converts an address and network mask to it's CIDR representation.
+    Converts an IPv4 or IPv6 prefix writen in various formats to its CIDR representation.
+
+    This filter works only on prefixes. Use normalize_address if you wish to normalize an address
+    without a network mask.
 
     Examples:
-        >>> "{{ '192.168.0.0 255.255.255.0'|addrmask_to_cidr }}" -> "192.168.0.0/24"
+        >>> "{{ '192.168.0.0 255.255.255.0'|normalize_prefix }}" -> "192.168.0.0/24"
+        >>> "{{ '192.168/255.255.255.0'|normalize_prefix }}" -> "192.168.0.0/24"
+        >>> "{{ '2001:DB8:0:0:1:0:0:1/64'|normalize_prefix }}" -> "2001:db8::1:0:0:1/64"
     """
     value = value.replace(' ', '/')
     return str(netaddr.IPNetwork(value))
 
+
+def normalize_address(value):
+    """
+    Converts an IPv4 or IPv6 address writen in various formats to a standard textual representation.
+
+    This filter works only on addresses without network mask. Use normalize_prefix to normalize
+    networks.
+
+    Examples:
+        >>> "{{ '192.168.0.1'|normalize_address }}" -> "192.168.0.1"
+        >>> "{{ '192.168.1'|normalize_address }}" -> "192.168.0.1"
+        >>> "{{ '2001:DB8:0:0:1:0:0:1'|normalize_address }}" -> "2001:db8::1:0:0:1"
+
+    """
+    return str(netaddr.IPAddress(value))
