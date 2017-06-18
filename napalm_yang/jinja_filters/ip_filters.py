771e1d34c3966e2637ee70d973cf8e5f7dbe642a
Matej Vadnjal <matej@vadnjal.net>
2017-06-18 16:44:23 +0200

    Add normalize_address normalize_prefix Jinja filters (#64)
    * add normalize_address jinja filter and rename addrmask_to_cidr to normalize_prefix
    
    * add unit tests for normalize_prefix and normalize_address
    
    * add unit tests fot the rest of ip_filters
    
    * add filter prefix_to_addrmask that curns CIDR represented subnets into address mask representation
    
    * make cidr_to_netmask return str

--- napalm_yang/jinja_filters/ip_filters.py
+++ napalm_yang/jinja_filters/ip_filters.py
@@ -5,7 +5,9 @@ def filters():
     return {
         "netmask_to_cidr": netmask_to_cidr,
         "cidr_to_netmask": cidr_to_netmask,
-        "addrmask_to_cidr": addrmask_to_cidr,
+        "normalize_prefix": normalize_prefix,
+        "normalize_address": normalize_address,
+        "prefix_to_addrmask": prefix_to_addrmask,
     }
 
 
@@ -26,16 +28,49 @@ def cidr_to_netmask(value):
     Examples:
         >>> "{{ '24'|cidr_to_netmask }}" -> "255.255.255.0"
     """
-    return netaddr.IPNetwork("1.1.1.1/{}".format(value)).netmask
+    return str(netaddr.IPNetwork("1.1.1.1/{}".format(value)).netmask)
 
 
-def addrmask_to_cidr(value):
+def normalize_prefix(value):
     """
-    Converts an address and network mask to it's CIDR representation.
+    Converts an IPv4 or IPv6 prefix writen in various formats to its CIDR representation.
+
+    This filter works only on prefixes. Use normalize_address if you wish to normalize an address
+    without a network mask.
 
     Examples:
-        >>> "{{ '192.168.0.0 255.255.255.0'|addrmask_to_cidr }}" -> "192.168.0.0/24"
+        >>> "{{ '192.168.0.0 255.255.255.0'|normalize_prefix }}" -> "192.168.0.0/24"
+        >>> "{{ '192.168/255.255.255.0'|normalize_prefix }}" -> "192.168.0.0/24"
+        >>> "{{ '2001:DB8:0:0:1:0:0:1/64'|normalize_prefix }}" -> "2001:db8::1:0:0:1/64"
     """
     value = value.replace(' ', '/')
     return str(netaddr.IPNetwork(value))
 
+
+def normalize_address(value):
+    """
+    Converts an IPv4 or IPv6 address writen in various formats to a standard textual representation.
+
+    This filter works only on addresses without network mask. Use normalize_prefix to normalize
+    networks.
+
+    Examples:
+        >>> "{{ '192.168.0.1'|normalize_address }}" -> "192.168.0.1"
+        >>> "{{ '192.168.1'|normalize_address }}" -> "192.168.0.1"
+        >>> "{{ '2001:DB8:0:0:1:0:0:1'|normalize_address }}" -> "2001:db8::1:0:0:1"
+
+    """
+    return str(netaddr.IPAddress(value))
+
+
+def prefix_to_addrmask(value, sep=' '):
+    """
+    Converts a CIDR formatted prefix into an address netmask representation.
+    Argument sep specifies the separator between the address and netmask parts. By default it's a single space.
+
+    Examples:
+        >>> "{{ '192.168.0.1/24|prefix_to_addrmask }}" -> "192.168.0.1 255.255.255.0"
+        >>> "{{ '192.168.0.1/24|prefix_to_addrmask('/') }}" -> "192.168.0.1/255.255.255.0"
+    """
+    prefix = netaddr.IPNetwork(value)
+    return '{}{}{}'.format(prefix.ip, sep, prefix.netmask)
