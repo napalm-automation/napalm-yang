f0c7b4572e6345dbf44e1c4889757ccb5fe38743
David Barroso <dbarrosop@dravetech.com>
2017-07-23 09:02:51 +0200

    Separate tests by config/state

--- test/integration/test_profiles.py
+++ test/integration/test_profiles.py
@@ -35,25 +35,26 @@ BASE_PATH = os.path.dirname(__file__)
 test_parse_models = [
     ["ios", "config", napalm_yang.models.openconfig_interfaces, "default"],
     ["eos", "config", napalm_yang.models.openconfig_network_instance, "default"],
+    ["eos", "config", napalm_yang.models.openconfig_interfaces, "default"],
     ["eos", "state", napalm_yang.models.openconfig_interfaces, "default"],
     ["junos", "config", napalm_yang.models.openconfig_network_instance, "default"],
     ["junos", "state", napalm_yang.models.openconfig_interfaces, "default"],
 ]
 
 
-def read_file_content(base, profile, model, case, filename):
+def read_file_content(base, profile, model, mode, case, filename):
     full_path = os.path.join(BASE_PATH, base,
-                             profile, model._yang_name, case, filename)
+                             profile, model._yang_name, mode, case, filename)
     with open(full_path, "r") as f:
         return f.read()
 
 
-def read_json(base, profile, model, case, filename):
-    return json.loads(read_file_content(base, profile, model, case, filename))
+def read_json(base, profile, model, mode, case, filename):
+    return json.loads(read_file_content(base, profile, model, mode, case, filename))
 
 
-def load_json_model(base, profile, model, case, filename):
-    expected_json = read_json(base, profile, model, case, "expected.json")
+def load_json_model(base, profile, model, mode, case, filename):
+    expected_json = read_json(base, profile, model, mode, case, "expected.json")
     expected = napalm_yang.base.Root()
     expected.add_model(model)
     expected.load_dict(expected_json)
@@ -64,11 +65,11 @@ class Tests(object):
 
     @pytest.mark.parametrize("profile, mode, model, case", test_parse_models)
     def test_parse(self, profile, mode, model, case):
-        expected = load_json_model("test_profiles", profile, model, case, "expected.json")
+        expected = load_json_model("test_profiles", profile, model, mode, case, "expected.json")
 
         optional_args = {
             "path": os.path.join(BASE_PATH, "test_profiles",
-                                 profile, model._yang_name, case, "mocked"),
+                                 profile, model._yang_name, mode, case, "mocked"),
             "profile": profile if isinstance(profile, list) else [profile],
         }
         with MockDriver("hostname", "username", "password", optional_args=optional_args) as d:
@@ -89,8 +90,8 @@ class Tests(object):
         if mode == "state":
             return
 
-        json_blob = read_json("test_profiles", profile, model, case, "expected.json")
-        expected_translation = read_file_content("test_profiles", profile, model, case,
+        json_blob = read_json("test_profiles", profile, model, mode, case, "expected.json")
+        expected_translation = read_file_content("test_profiles", profile, model, mode, case,
                                                  "translation.txt")
 
         config = napalm_yang.base.Root()
@@ -112,10 +113,10 @@ class Tests(object):
         if mode == "state":
             return
 
-        json_running = read_json("test_profiles", profile, model, case, "expected.json")
-        json_candidate = read_json("test_profiles", profile, model, case, "candidate.json")
+        json_running = read_json("test_profiles", profile, model, mode, case, "expected.json")
+        json_candidate = read_json("test_profiles", profile, model, mode, case, "candidate.json")
 
-        expected_translation = read_file_content("test_profiles", profile, model, case,
+        expected_translation = read_file_content("test_profiles", profile, model, mode, case,
                                                  "{}.txt".format(action))
 
         candidate = napalm_yang.base.Root()
