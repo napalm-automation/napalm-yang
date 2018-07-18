f75cc912e5495576485f281fc941abacf9a620e2
Carles Kishimoto <carles.kishimoto@gmail.com>
2018-07-18 16:29:37 +0200

    junos parser fix test case

--- test/integration/test_profiles.py
+++ test/integration/test_profiles.py
@@ -45,6 +45,7 @@ test_parse_models = [
     ["eos", "state", napalm_yang.models.openconfig_interfaces, "default"],
     ["junos", "config", napalm_yang.models.openconfig_interfaces, "default"],
     ["junos", "config", napalm_yang.models.openconfig_network_instance, "default"],
+    ["junos", "config", napalm_yang.models.openconfig_network_instance, "ospf"],
     ["junos", "config", napalm_yang.models.openconfig_system, "default"],
     ["junos", "state", napalm_yang.models.openconfig_interfaces, "default"],
     ["nxos", "config", napalm_yang.models.openconfig_interfaces, "default"],
